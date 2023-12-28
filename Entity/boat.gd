d2fa218092f09a0eed38d8734cd19ec66a2c6eca
lucas <lucas.zorman@telecomnancy.net>
2023-12-28 14:58:43 +0100

    Boats as statics body

--- Entity/boat.gd
+++ Entity/boat.gd
@@ -1,18 +1,22 @@
-extends RigidBody2D
+class_name Boat extends RigidBody2D
 
 
 const SPEED = 200.0
 var angular_force = 2500
 var target = null
+var initial_dir = Vector2(1,0)
 
 func _ready():
 	linear_velocity = Vector2(200,0)
 	
 	
 func _physics_process(delta):
-	if target != null :
+	if target != null and target.active:
 		var dir = transform.y.dot(position.direction_to(target.position))
 		apply_torque(dir * angular_force)
-
+	else :
+		var dir = transform.y.dot(initial_dir)
+		apply_torque(dir * angular_force)
+		
 func set_target(newt : CharacterBody2D):
 	target = newt

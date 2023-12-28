fc335079b5ccf5c732d978d075b89370d00f96f5
library-rat <1ucas_z@protonmail.com>
2023-12-28 17:34:24 +0100

    ajout nageur

--- main.gd
+++ main.gd
@@ -1,15 +1,21 @@
 extends Node2D
 
-
+var score : int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	randomize()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
+func add_nageur(nageur,boat_position):
+	add_child(nageur)
+	nageur.connect("eaten",_on_nageur_eaten)
+	nageur.position = boat_position
+	
+func _on_nageur_eaten():
+	print("yum yum")
 
 func _on_sirene_gameover():
 	get_tree().paused = true

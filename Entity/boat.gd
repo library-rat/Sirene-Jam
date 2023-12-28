e9ac2f77303ff73729111690f51b0c188e9676aa
lucas <lucas.zorman@telecomnancy.net>
2023-12-28 15:37:09 +0100

    setting boat as character2D

--- Entity/boat.gd
+++ Entity/boat.gd
@@ -1,22 +1,23 @@
-class_name Boat extends RigidBody2D
+class_name Boat extends CharacterBody2D
 
-
-const SPEED = 200.0
-var angular_force = 2500
+var speed = Vector2(200,0);
+var angular_speed = 0.4
 var target = null
-var initial_dir = Vector2(1,0)
+var initial_dir = 0
 
-func _ready():
-	linear_velocity = Vector2(200,0)
+	
 	
 	
 func _physics_process(delta):
+
 	if target != null and target.active:
-		var dir = transform.y.dot(position.direction_to(target.position))
-		apply_torque(dir * angular_force)
+		rotation = rotate_toward(rotation, position.angle_to_point(target.position), 2*angular_speed*delta)
 	else :
-		var dir = transform.y.dot(initial_dir)
-		apply_torque(dir * angular_force)
+		rotation = rotate_toward(rotation, initial_dir, angular_speed*delta)
+	velocity = speed.rotated(rotation)
+	
+	move_and_slide()
+	
 		
 func set_target(newt : CharacterBody2D):
 	target = newt

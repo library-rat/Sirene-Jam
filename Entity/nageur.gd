5b3c428a9d3777868a6a386fef7a70a62628e411
library-rat <1ucas_z@protonmail.com>
2023-12-29 14:49:34 +0100

    adding some graphics

--- Entity/nageur.gd
+++ Entity/nageur.gd
@@ -1,6 +1,7 @@
 extends RigidBody2D
-
+var dead : bool = false
 signal eaten()
+signal blood(pos : Vector2) 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("hello")
@@ -12,6 +13,18 @@ func _process(delta):
 
 
 func _on_area_2d_body_entered(body):
-	if body.is_in_group("Sirene") :
+	if body.is_in_group("Sirene") and !dead:
 		emit_signal("eaten") 
-		queue_free()
+	dead = true
+	emit_signal("blood", position)
+	$"Sprite2D".visible = false
+	$"Blood".visible = true
+	$Timer.start(10)
+	
+
+
+
+
+
+func _on_timer_timeout():
+	queue_free()
--- Entity/sirene.gd
+++ Entity/sirene.gd
@@ -3,6 +3,7 @@ class_name sirene extends CharacterBody2D
 
 const SPEED = 300.0
 var active : bool = true;
+var onland : bool = false;
 signal gameover()
 
 func _physics_process(delta):
@@ -20,24 +21,44 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y,0, SPEED)
+	
+	if direction != Vector2.ZERO:
+		rotation =  PI+ direction.angle()
+	
 	var collider  = move_and_collide(velocity*delta)
 	
 	if collider != null :
 		emit_signal("gameover")
+		
 	
 	if (velocity == Vector2.ZERO):
 		active = true;
+		if(onland):
+			$Sprite2D_Land/Song.visible = true
+		else :
+			$Sprite2D_Sea/Song.visible = true
 		$Area2D.gravity_space_override = Area2D.SPACE_OVERRIDE_COMBINE
 		
 	else:
 		active = false;
+		if(onland):
+			$Sprite2D_Land/Song.visible = false
+		else :
+			$Sprite2D_Sea/Song.visible = false
 		$Area2D.gravity_space_override = Area2D.SPACE_OVERRIDE_DISABLED
 
+
 func touch_land():
+	onland = true
 	$"Area2D/CollisionShape2D".shape.radius *= 1.5
+	$"Sprite2D_Land".visible = true
+	$"Sprite2D_Sea".visible = false
 
 func touch_sea():
+	onland = false
 	$"Area2D/CollisionShape2D".shape.radius /= 1.5
+	$"Sprite2D_Land".visible = false
+	$"Sprite2D_Sea".visible = true
 
 
 func _on_area_2d_body_entered(body):

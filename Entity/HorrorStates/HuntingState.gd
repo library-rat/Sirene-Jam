48e295d164a3c6f6dd919f02ab99a2168543e7d5
library-rat <1ucas_z@protonmail.com>
2023-12-30 16:11:13 +0100

    adding following to horror

--- Entity/HorrorStates/HuntingState.gd
+++ Entity/HorrorStates/HuntingState.gd
@@ -1,12 +1,18 @@
 extends State
 
-var hunt_speed = 400
+var hunt_speed = 210
 
 func onSetState():
 	$"..".set_Sprite("Hunting")
 
 func state_process(delta):
 	if hunter.target == null :
-		hunter
+		hunter.set_state("Resting")
+	else :
+		var direction = hunter.position.direction_to(hunter.target.position)
+		hunter.velocity = direction*hunt_speed
+		if hunter.target.is_in_group("Sirene"):
+			if hunter.target.onland :
+				hunter.target = null
 	
 
